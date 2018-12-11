f85d0484f88434547ca36434e38db91beafa27c2
BagboBillbins <33336191+BagboBillbins@users.noreply.github.com>
2018-12-10 19:46:47 -0600

    added animations for all menus and changed quit and load functions to an animation with a new script

---
+++ Assets/Scripts/LoadOnEnter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnEnter : StateMachineBehaviour {
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
